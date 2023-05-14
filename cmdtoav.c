778382bb0f3ecabbb7a425f754420ca2556d6c6f
onyxvail <ons.hfi@gmail.com>
2023-05-14 18:12:31 +0100

    fix

--- cmdtoav.c
+++ cmdtoav.c
@@ -1,11 +1,21 @@
 #include "shell.h"
 
-int main(int argc, char **argv) {
-  char **words = split_string(argv[0], " ");
-  int i;
-  for (i = 0; words[i] != NULL; i++) {
-    printf("%s\n", words[i]);
-  }
-  free(words);
-  return 0;
+/**
+ * main - Entry point for the program
+ *
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of string arguments passed to the program
+ *
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+char **words = split_string(argv[0], " ");
+int i;
+for (i = 0; words[i] != NULL; i++)
+{
+printf("%s\n", words[i]);
+}
+free(words);
+return (0);
 }
