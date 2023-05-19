67341d2cd21f486cad977465205b95b8e379a805
onyxvail <ons.hfi@gmail.com>
2023-05-19 12:39:06 +0100

    update

--- TEST/main.c
+++ TEST/main.c
@@ -2,28 +2,29 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-int main() {
-  char *command = NULL;
-  size_t bufsize = 0;
-  ssize_t getline_result;
-  int status;
 
-  while (1) {
-    printf("simple_shell> ");
-    fflush(stdout);
+int main(void)
+{
+char *command = NULL;
+size_t bufsize = 0;
+ssize_t getline_result;
+int status;
 
-    getline_result = getline(&command, &bufsize, stdin);
-    if (getline_result == -1) {
-      break;
-    }
-
-    status = system(command);
-    if (status != 0) {
-      printf("Error executing command: %d\n", status);
-    }
-
-    
-  }
-    free(command);
-    return 0;
+while (1)
+{
+printf("simple_shell> ");
+fflush(stdout);
+getline_result = getline(&command, &bufsize, stdin);
+if (getline_result == -1)
+{
+break;
+}
+status = system(command);
+if (status != 0)
+{
+printf("Error executing command: %d\n", status);
+}
+}
+free(command);
+return (0);
 }
