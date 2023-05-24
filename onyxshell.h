ef641cb155a3ea9e01f86a29e18387d574f0c8c9
pandonyx <pandonyx@gmail.com>
2023-05-24 23:14:40 +0100

    por favor senior julien

--- onyxshell.h
+++ onyxshell.h
@@ -64,36 +64,36 @@ typedef struct builtin
 	char *cmd;
 	int (*f)(shl_t *data);
 } blt_t;
-/* ----------Process prototype------------*/
+
 int reading_line(shl_t *);
 int spliting_line(shl_t *);
 int parsing_line(shl_t *);
 int processing_cmd(shl_t *);
 
-/* ----------String prototype------------*/
+
 char *_strdupl(char *str);
 char *_strcatt(char *fst, char *snd);
 int _strlen(char *str);
 char *_strchr(char *str, char c);
 int _strcmp(char *s1, char *s2);
 
-/* ----------More String prototype-------*/
+
 char *_strcpyy(char *destn, char *source);
 
-/* ----------Memory prototype------------*/
+
 void *_reallocate(void *ptr, unsigned int o_size, unsigned int n_size);
 char *_memsett(char *s, char byt, unsigned int n);
 char *_memcpyy(char *destn, char *srcs, unsigned int nb);
 int free_datas(shl_t *);
 
-/* ----------Tools prototype-------------*/
+
 void *fill_arr(void *ar, int elm, unsigned int leng);
 void sig_handler(int sig);
 char *_getenv(char *path_nm);
 void index_cmds(shl_t *datas);
 void array_revs(char *arrs, int leng);
 
-/* ----------More tools prototype--------*/
+
 char *_itoa(unsigned int nt);
 int intleng(int numb);
 int _atoi(char *c);
@@ -101,14 +101,14 @@ int printing_error(shl_t *data);
 int write_hist(shl_t *data);
 int _isitalpha(int cc);
 
-/* -------------Builtins-----------------*/
+
 int aborting_prgm(shl_t *data);
 int changing_dir(shl_t *data);
 int displaying_help(shl_t *data);
 int handling_builtins(shl_t *data);
 int check_builtin(shl_t *data);
 
-/* -------------Parse-----------------*/
+
 int ispath_form(shl_t *data);
 void isshort_form(shl_t *data);
 int is_builtin(shl_t *data);
