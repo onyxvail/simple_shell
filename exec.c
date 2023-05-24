ff75a95919578531f512371a4e4fcec3e70fe71b
pandonyx <pandonyx@gmail.com>
2023-05-24 20:50:58 +0100

    betty

--- exec.c
+++ exec.c
@@ -12,31 +12,29 @@
  */
 void execute_command(char *args[], int num_args, char *program_name)
 {
-    pid_t pid = fork();
+pid_t pid = fork();
 
-    (void)num_args;
+(void)num_args;
 
-    
+if (pid == -1)
+{
+perror("fork");
+exit(1);
+}
+else if (pid == 0)
+{
+if (execvp(args[0], args) == -1)
+{
+fprintf(stderr, "%s: command not found: %s\n", program_name, args[0]);
+exit(1);
+}
+}
+else
+{
+int status;
+waitpid(pid, &status, 0);
 
-    if (pid == -1)
-    {
-        perror("fork");
-        exit(1);
-    }
-    else if (pid == 0)
-    {
-        if (execvp(args[0], args) == -1)
-        {
-            fprintf(stderr, "%s: command not found: %s\n", program_name, args[0]);
-            exit(1);
-        }
-    }
-    else
-    {
-        int status;
-        waitpid(pid, &status, 0);
-
-        if (!isatty(STDIN_FILENO))
-        printf("($)\n");
-    }
+if (!isatty(STDIN_FILENO))
+printf("($)\n");
+}
 }
